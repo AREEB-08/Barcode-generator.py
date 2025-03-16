6daaf06e1267c70ffb164608660700235b4f9b36
Mohammad Areeb uddin <mohammadareeb411@gmail.com>
2025-03-16 23:16:27 +0530

    qr_code_generator.py

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,1 +1,14 @@
 
+import qrcode
+from PIL import Image, ImageDraw, ImageFont
+
+# User input for the link to generate the QR code
+link = input("Enter the link you want to create a QR code for: ")
+
+# Create a QR Code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=8,
+    border=3,
+)
